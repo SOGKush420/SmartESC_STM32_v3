d958e614cae2795ec3cdfa0499eff57e2999b676
SOGKush420 <41782990+SOGKush420@users.noreply.github.com>
2022-05-28 18:21:36 +0200

    Speed13

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -43,13 +43,13 @@
 #define BRAKEMAX 190
 
 // parameters for speed calculation
-#define WHEEL_CIRCUMFERENCE 550 //690 for original M365 motor
-#define GEAR_RATIO 11 //15 for original M365 motor
+#define WHEEL_CIRCUMFERENCE 690 //690 for original M365 motor
+#define GEAR_RATIO 15 //15 for original M365 motor
 
 // speed limits for invividual modes in kph
 #define SPEEDLIMIT_ECO 20
-#define SPEEDLIMIT_NORMAL 40
-#define SPEEDLIMIT_SPORT 50
+#define SPEEDLIMIT_NORMAL 50
+#define SPEEDLIMIT_SPORT 70
 
 // motor current limits for invividual modes in mA, see default settings at https://max.cfw.sh/#
 #define PH_CURRENT_MAX_ECO 16000
@@ -64,7 +64,7 @@
 
 // maximum battery currents in mA
 #define BATTERYCURRENT_MAX 14500
-#define REGEN_CURRENT_MAX 10000
+#define REGEN_CURRENT_MAX 15000
 
 // battery voltage limits in mV
 #define BATTERYVOLTAGE_MIN 33000
@@ -76,7 +76,7 @@
 
 // settings for speed PLL (angle estimation)
 #define SPEED_PLL 1 //1 for using PLL, 0 for angle extrapolation
-#define P_FACTOR_PLL 10 //7 for original M365 motor
-#define I_FACTOR_PLL 10 //7 for original M365 motor
+#define P_FACTOR_PLL 7 //7 for original M365 motor
+#define I_FACTOR_PLL 7 //7 for original M365 motor
 
 #endif /* CONFIG_H_ */
